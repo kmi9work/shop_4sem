1edfea20c24221ba25fcce94d12651d7c9ccf15b
Михаил Костенчук <MichaelKostekchuk@MacBookMK-7.local>
2010-05-18 00:40:53 +0400

    added data

--- config/routes.rb
+++ config/routes.rb
@@ -43,4 +43,5 @@ ActionController::Routing::Routes.draw do |map|
 
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+  map.root :controller => 'items', :action => 'list', :user_name => 'operator'
 end
